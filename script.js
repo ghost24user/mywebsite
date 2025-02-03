b6e7c38cfd9de901fe64f71d37504fad7e10ba90
ghost24user <sebastian.ungureanu9@icloud.com>
2025-02-03 20:29:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function contact() {
     // Example: Open an email link
-    window.location.href = "https://wa.me/40743522833?text=Bun%C4%83%2C%20sunt%20interesat%2F%C4%83%20de%20serviciile%20tale.
-";
+    window.location.href = "https://wa.me/40743522833;
 }
